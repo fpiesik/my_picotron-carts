2cf672aa2b927a854a5766d7e76a7aba5f745b5f
Frank Piesik <f.piesik@gmail.com>
2026-04-13 10:52:31 +0200

    ...

---
+++ fonts/umlaute.lua
@@ -0,0 +1,11 @@
+--[[pod_format="raw",created="2026-04-13 08:19:07",modified="2026-04-13 08:19:07",revision=0]]
+font_config = {
+	width = 5,
+	width_128 = 10,
+	height = 11,
+	x_offset = 0,
+	y_offset = 0,
+	flags = 0,
+	char_offsets = {
+	}
+}
